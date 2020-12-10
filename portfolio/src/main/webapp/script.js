3b37fd72848b3e00a67a993e09b6ed2443602679
Helena Tran <helena.e.tran@student.uts.edu.au>
2020-12-10 22:27:06 +0000

    Make nav bar functional and fix indentation issues

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,9 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Initialise the slider (index.html) and the material box (photoGallery.html)
 document.addEventListener('DOMContentLoaded', function() {
-    elems = document.querySelectorAll('.slider');
-    instances = M.Slider.init(elems);
+  elems_one = document.querySelectorAll('.slider');
+  instances_one = M.Slider.init(elems_one);
+  elems_two = document.querySelectorAll('.materialboxed');
+  instances_two = M.Materialbox.init(elems_two);
 });
 
 /**
@@ -22,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+    ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
@@ -36,66 +39,66 @@ function addRandomGreeting() {
  * Add a random fact about me to the page
  */
 function addRandomFact() {
-    const facts = [
-        'I am a foodie',
-        'I have a collection of Starbucks mugs from around the world',
-        'I have a collection of "I LOVE" T-shirt from around the world',
-        'I have travelled to over 15 different countries',
-        'I have never broken anything on my body',
-        'I used to do Karate when I was younger',
-        'I watched all the Harry Potters in 2019 because I never watched them before (I know, shame on me!)'
-    ];
+  const facts = [
+    'I am a foodie',
+    'I have a collection of Starbucks mugs from around the world',
+    'I have a collection of "I LOVE" T-shirt from around the world',
+    'I have travelled to over 15 different countries',
+    'I have never broken anything on my body',
+    'I used to do Karate when I was younger',
+    'I watched all the Harry Potters in 2019 because I never watched them before (I know, shame on me!)'
+  ];
 
-    //Pick a random fact
-    const fact = facts[Math.floor(Math.random() * facts.length)];
+  //Pick a random fact
+  const fact = facts[Math.floor(Math.random() * facts.length)];
 
-    //Add it to the page
-    const factContainer = document.getElementById('fact-container');
-    factContainer.innerText = fact;
+  //Add it to the page
+  const factContainer = document.getElementById('fact-container');
+  factContainer.innerText = fact;
 }
 
 /**
  * Display the 'About Me' section
  */
 function getAboutMe(aboutMeId) {
-    if (aboutMeId === 'hobbies') {
-        elemId = 'hobbies-container';
-    }
-    if (aboutMeId === 'loves') {
-        elemId = 'loves-container';
-    }
-    if (aboutMeId === 'hates') {
-        elemId = 'hates-container';
-    }
+  if (aboutMeId === 'hobbies') {
+    elemId = 'hobbies-container';
+  }
+  if (aboutMeId === 'loves') {
+    elemId = 'loves-container';
+  }
+  if (aboutMeId === 'hates') {
+    elemId = 'hates-container';
+  }
 
-    const aboutMeContainer = document.getElementById('about-me-container');
-    const subContainer = document.getElementById(elemId);
+  const aboutMeContainer = document.getElementById('about-me-container');
+  const subContainer = document.getElementById(elemId);
 
-    const hobbiesContainer = document.getElementById('hobbies-container');
-    const lovesContainer = document.getElementById('loves-container');
-    const hatesContainer = document.getElementById('hates-container');
+  const hobbiesContainer = document.getElementById('hobbies-container');
+  const lovesContainer = document.getElementById('loves-container');
+  const hatesContainer = document.getElementById('hates-container');
 
-    if (aboutMeContainer.style.display === 'none') {
-        aboutMeContainer.style.display = 'block';
-        subContainer.style.display = 'block';
-    }
-    else if(aboutMeContainer.style.display !== 'none' && subContainer.style.display === 'none') {
-        subContainer.style.display = 'block';
-            if (aboutMeId === 'hobbies') {
-                lovesContainer.style.display = 'none';
-                hatesContainer.style.display = 'none';
-            }
-            if (aboutMeId === 'loves') {
-                hobbiesContainer.style.display = 'none';
-                hatesContainer.style.display = 'none';
-            }
-            if (aboutMeId === 'hates') {
-                hobbiesContainer.style.display = 'none';
-                lovesContainer.style.display = 'none';
-            }
-    }
-    else {
-        aboutMeContainer.style.display = 'none';
-        subContainer.style.display = 'none';
-    }
+  if (aboutMeContainer.style.display === 'none') {
+    aboutMeContainer.style.display = 'block';
+    subContainer.style.display = 'block';
+  }
+  else if(aboutMeContainer.style.display !== 'none' && subContainer.style.display === 'none') {
+    subContainer.style.display = 'block';
+      if (aboutMeId === 'hobbies') {
+        lovesContainer.style.display = 'none';
+        hatesContainer.style.display = 'none';
+      }
+      if (aboutMeId === 'loves') {
+        hobbiesContainer.style.display = 'none';
+        hatesContainer.style.display = 'none';
+      }
+      if (aboutMeId === 'hates') {
+        hobbiesContainer.style.display = 'none';
+        lovesContainer.style.display = 'none';
+      }
+  }
+  else {
+    aboutMeContainer.style.display = 'none';
+    subContainer.style.display = 'none';
+  }
 }
