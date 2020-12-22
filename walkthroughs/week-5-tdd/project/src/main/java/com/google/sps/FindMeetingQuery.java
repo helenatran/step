7959ff5c09033492bb09933744dc4401a18cef18
Helena Tran <helena.e.tran@student.uts.edu.au>
2020-12-22 04:35:01 +0000

    Add different scenarios (not general yet) to pass the test code

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public final class FindMeetingQuery {
   // Return the times when the meeting could happen that day.
@@ -33,25 +34,80 @@ public final class FindMeetingQuery {
     }
 
     /**
-     * If there is an event during the day, the possible times are before and after the event.
+     * If there is an event during the day, there are 
      * ToDo(Helena): consider when there are more than 1 event
      */
     if (events.size() == 1) {
       Collection<TimeRange> possibleTimes = new ArrayList<>();
-      for (Event event : events) {
-        possibleTimes.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, event.getWhen().start(), false));
-        possibleTimes.add(TimeRange.fromStartEnd(event.getWhen().end(), TimeRange.END_OF_DAY, true));
+      Iterator<Event> eventIt = events.iterator();
+      Event onlyEvent = eventIt.next();
+      Iterator<String> eventAttendeeIt = onlyEvent.getAttendees().iterator();
+      String eventAttendee = eventAttendeeIt.next();
+
+      // If the event is linked to a non-required attendee, the whole day should be available.
+      Iterator<String> requestAttendeeIt = request.getAttendees().iterator();
+      String requiredAttendee = requestAttendeeIt.next();
+
+      if (eventAttendee != requiredAttendee) {
+        return Arrays.asList(TimeRange.WHOLE_DAY);
       }
-      return possibleTimes;
+
+      // If the event is linked to a required attendee, there are 2 possible times: before and after the event.
+      else {
+        possibleTimes.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, onlyEvent.getWhen().start(), false));
+        possibleTimes.add(TimeRange.fromStartEnd(onlyEvent.getWhen().end(), TimeRange.END_OF_DAY, true));
+        return possibleTimes;
+      }
     }
 
     /**
-     * If there are 2 attendees, the possible times are the ones in-between their events.
+     * If there are 2 events linked to required attendees, there are multiple scenarios possible:
      * ToDo(Helena):
-     *  - First, implement the function as stated above, then: 
      *  - Consider when there are more than 2 attendees.
      *  - Consider when they have more than 1 event per attendee.
+     * We assume that events are always added chronologically (based on their start time).
      */
+    if (events.size() == 2) {
+      Collection<TimeRange> possibleTimes = new ArrayList<>();
+      Iterator<Event> it = events.iterator();
+      Event firstEvent = it.next();
+      Event secondEvent = it.next();
+
+      // If one person's event fully contains another's event (nested events), there are 2 possible times: before and after the first event.
+      if (firstEvent.getWhen().end() > secondEvent.getWhen().start()
+          && firstEvent.getWhen().end() > secondEvent.getWhen().end()) {
+        possibleTimes.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, firstEvent.getWhen().start(), false));
+        possibleTimes.add(TimeRange.fromStartEnd(firstEvent.getWhen().end(), TimeRange.END_OF_DAY, true));
+        return possibleTimes;
+      }
+
+      // If the events are overlapping, there are 2 possible times: before the first event and after the second event.
+      else if (firstEvent.getWhen().end() > secondEvent.getWhen().start()) {
+        possibleTimes.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, firstEvent.getWhen().start(), false));
+        possibleTimes.add(TimeRange.fromStartEnd(secondEvent.getWhen().end(), TimeRange.END_OF_DAY, true));
+        return possibleTimes;
+      }
+
+      /**
+       * If the events are not overlapping but the first meeting starts at the start of the day and
+       * the second meeting ends at the end of the day, we have 1 possible time: between these events.
+       */
+      else if (firstEvent.getWhen().start() == TimeRange.START_OF_DAY
+          && secondEvent.getWhen().end() == TimeRange.END_OF_DAY+1) {
+        int duration = Math.toIntExact(request.getDuration());
+        possibleTimes.add(TimeRange.fromStartDuration(firstEvent.getWhen().end(), duration));
+        return possibleTimes;
+      }
+
+      // If the events are not overlapping and start/end in the middle of the day, there 3 possible times (in-between the events).
+      else {
+        possibleTimes.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, firstEvent.getWhen().start(), false));
+        possibleTimes.add(TimeRange.fromStartEnd(firstEvent.getWhen().end(), secondEvent.getWhen().start(), false));
+        possibleTimes.add(TimeRange.fromStartEnd(secondEvent.getWhen().end(), TimeRange.END_OF_DAY, true));
+        return possibleTimes;
+      }
+    }
+
     throw new UnsupportedOperationException("TODO: Implement this method.");
   }
 }
